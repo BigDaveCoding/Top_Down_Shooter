f94eb6b453022adf60b4ca68b6819c358e3da59f
BigDaveCoding <bigdavecoding@gmail.com>
2024-05-22 22:32:43 +0100

    added a boolean to say whether or not the player can move. Thinking of moving into a seperate script which will contain multiple variables determining the status of the player. for example whether they can move, are they in the inventory screen, have they pressed pause etc.

--- Scripts/Player/Player_Controller.cs
+++ Scripts/Player/Player_Controller.cs
@@ -7,21 +7,28 @@ public class Player_Controller : MonoBehaviour
 {
     public float moveSpeed = 1f; // move speed which can be assigned in the unity inspector.
     [SerializeField] private Vector2 moveDirection; // private variable to assign vector2 in OnMove function
-    [SerializeField] private Rigidbody2D _rb; // private rigidbody which will be assigned in start 
+    [SerializeField] private Rigidbody2D _rb; // private rigidbody which will be assigned in start
+
+    public bool playerCanMove; // bool variable which will be used later on to control whether the player is allowed to move or not
+    //could move this bool into its own script which keeps track of player variables needed to be altered by many different scripts
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>(); //assigning rigidbody2d attached to gameObject the script is on.
-        
+        playerCanMove = true; // giving bool the value of true as it sets itself to false unless specified. at the moment there is nothing to stop the player from moving.
     }
 
     //fixed update calls once per frame.
     private void FixedUpdate()
     {
-        //player movement
-        Vector2 movement = moveDirection * moveSpeed * Time.fixedDeltaTime; //creating instance variable to store vector2 movement.
-        _rb.position += movement; // adding the movement variable to the rigidbody2d position co ordinates
+        if (playerCanMove)
+        {
+            //player movement
+            Vector2 movement = moveDirection * moveSpeed * Time.fixedDeltaTime; //creating instance variable to store vector2 movement.
+            _rb.position += movement; // adding the movement variable to the rigidbody2d position co ordinates
+        }
+        
 
     }
 
