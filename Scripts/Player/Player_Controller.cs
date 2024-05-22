71f78e6a3a3978a992458d8e347a8fa2bceb992d
BigDaveCoding <bigdavecoding@gmail.com>
2024-05-22 22:16:59 +0100

    adding fixed update code to control the player movement from movedirection assigned by OnMove function. also added [serializefield] to the private variables so i can see/modify them in unity inspector for testing purposes

--- Scripts/Player/Player_Controller.cs
+++ Scripts/Player/Player_Controller.cs
@@ -6,19 +6,21 @@ using UnityEngine.InputSystem; // needed for the input system asset in unity
 public class Player_Controller : MonoBehaviour
 {
     public float moveSpeed = 1f; // move speed which can be assigned in the unity inspector.
-    private Vector2 moveDirection; // private variable to assign vector2 in OnMove function
-    private Rigidbody2D _rb; // private rigidbody which will be assigned in start 
+    [SerializeField] private Vector2 moveDirection; // private variable to assign vector2 in OnMove function
+    [SerializeField] private Rigidbody2D _rb; // private rigidbody which will be assigned in start 
 
     // Start is called before the first frame update
     void Start()
     {
+        _rb = GetComponent<Rigidbody2D>(); //assigning rigidbody2d attached to gameObject the script is on.
         
     }
 
     //fixed update calls once per frame.
     private void FixedUpdate()
     {
-        
+        //player movement
+
     }
 
     private void OnMove(InputValue value)
